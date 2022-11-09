e0fabcdde41367b3d49218877aedd9db28432dce
Diego <durbaneja3@gmail.com>
2022-11-09 15:17:25 -0500

    add projects

---
+++ packages/ui/Button.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+export const Button = ({
+  onClick,
+  children,
+}: {
+  onClick: () => void;
+  children?: React.ReactNode;
+}) => {
+  return <button onClick={onClick}>{children}</button>;
+};
